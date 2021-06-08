d5c8540fe6e6fc7b94a2dfd68a5613664e1327c6
vasfiop <zhang13898637960@outlook.com>
2021-06-08 12:25:28 +0800

    2021_6_8 12:25 更新

--- coffeeshop_navbar/src/main/java/com/example/coffeeshop_navbar/Activity/ShopActivity.java
+++ coffeeshop_navbar/src/main/java/com/example/coffeeshop_navbar/Activity/ShopActivity.java
@@ -1,16 +1,99 @@
 package com.example.coffeeshop_navbar.Activity;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.coffeeshop_navbar.R;
+import com.example.coffeeshop_navbar.db.DBUtil;
+import com.example.coffeeshop_navbar.value.Shop;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ShopActivity extends AppCompatActivity {
+    private ArrayList<HashMap<String, Object>> data;
+    private ListView listView;
+    private final String DB_DIR = "databases";
+    private final String DB_NAME = "coffeeshop";
+    private String databasePath;
+    private ApplicationInfo appInfo;
+    private DBUtil dbUtil;
+    private Shop[] shops;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        init(this);
+
+        set();
+    }
+
+    private void init(Context context) {
+        listView = findViewById(R.id.SA_listview);
+
+        String packageName = context.getPackageName();
+
+        try {
+            appInfo = context.getPackageManager().getApplicationInfo(
+                    packageName, PackageManager.GET_META_DATA);
+            String dbDir = appInfo.dataDir + File.separator + DB_DIR;
+            File file = new File(dbDir);
+            if (!file.exists()) {
+                file.mkdir();
+            }
+            databasePath = appInfo.dataDir + File.separator + DB_DIR
+                    + File.separator + DB_NAME;
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        dbUtil = DBUtil.getInstance(databasePath);
+        dbUtil.openDB();
+    }
+
+    private void set() {
+        shops = dbUtil.queryAllShops();
+        data = new ArrayList<>();
+        for (int i = 0; i < shops.length; i++) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("shop_name", shops[i].getShop_name());
+            map.put("shop_address", shops[i].getShop_address());
+            map.put("shop_tel", shops[i].getTel());
+            String img_name = shops[i].getImg_name();
+            int img_id = getResources().getIdentifier(img_name, "drawable", getPackageName());
+            map.put("img_id", img_id);
+
+            data.add(map);
+        }
+
+        MyAdapter adapter = new MyAdapter(this, data, R.layout.item_listview_shop, new String[]{"shop_name", "shop_address", "shop_tel", "img_id"}, new int[]{R.id.shop_name, R.id.shop_address, R.id.shop_tel, R.id.iv_img});
+        listView.setAdapter(adapter);
+    }
+
+    class MyAdapter extends SimpleAdapter {
+
+        public MyAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+            super(context, data, resource, from, to);
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return super.getView(position, convertView, parent);
+        }
     }
 }
--- coffeeshop_navbar/src/main/java/com/example/coffeeshop_navbar/fragment/HomeFragment.java
+++ coffeeshop_navbar/src/main/java/com/example/coffeeshop_navbar/fragment/HomeFragment.java
@@ -1,15 +1,18 @@
 package com.example.coffeeshop_navbar.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.coffeeshop_navbar.Activity.ShopActivity;
 import com.example.coffeeshop_navbar.R;
 import com.example.coffeeshop_navbar.org.taptwo.android.widget.CircleFlowIndicator;
 import com.example.coffeeshop_navbar.org.taptwo.android.widget.ViewFlow;
@@ -19,6 +22,7 @@ public class HomeFragment extends Fragment {
     private View view;
     private ViewFlow viewFlow;
     private CircleFlowIndicator circleFlowIndicator;
+    private LinearLayout linearLayout_1;
 
     @Nullable
     @Override
@@ -36,6 +40,7 @@ public class HomeFragment extends Fragment {
     private void init() {
         viewFlow = view.findViewById(R.id.viewFlow);
         circleFlowIndicator = view.findViewById(R.id.flowInficator);
+        linearLayout_1 = view.findViewById(R.id.ll_module1);
     }
 
     private void set() {
@@ -44,5 +49,18 @@ public class HomeFragment extends Fragment {
         viewFlow.setTimeSpan(4000);
         viewFlow.setSelection(3 * 1000);
         viewFlow.startAutoFlowTimer();
+        linearLayout_1.setOnClickListener(new ShopClickLinster());
+    }
+
+    class ShopClickLinster implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.ll_module1:
+                    Intent intent = new Intent(getActivity(), ShopActivity.class);
+                    startActivity(intent);
+            }
+        }
     }
 }
